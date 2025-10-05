ddea45355d2bbc053ce9ca119394e479764bf40c
Licaonchik <ereneger1609@gmail.com>
2025-10-05 11:45:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите конечное число");
+double n = Convert.ToDouble(Console.ReadLine());
+double g = 0;
+double i = 1;
+while (i <= n)
+{
+    g = i * 10 + 1;
+    i++;
+    
+    Console.WriteLine(g);
+
+} 
